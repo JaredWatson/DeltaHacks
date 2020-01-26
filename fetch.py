3d36c624307cb53879c24476e4ade9ef1aa28c89
Sparsh2234 <sparsh1411@gmail.com>
2020-01-26 10:53:35 -0500

    Final commit
    Final git commit

--- fetch.py
+++ fetch.py
@@ -1,47 +1,13 @@
 import pymysql
 import csv
 import sys
+import mysql.connector
 
+# dec = int (input ("1: Push 2:Pull   "))
 
-dec = int (input ("1: Push 2:Pull   "))
-
-
-if (dec == 2):
-    db_opts = {
-        'user': 'babysafe',
-        'password': 'FireFire',
-        'host': '165.227.33.15',
-        'database': 'babysafe'
-    }
-
-    db = pymysql.connect(**db_opts)
-    cur = db.cursor()
-
-    sql = 'SELECT * from data'
-    csv_file_path = 'C:/Users/Sparsh/Desktop/my_csv_file.csv'
-
-    try:
-        cur.execute(sql)
-        rows = cur.fetchall()
-    finally:
-        db.close()# Continue only if there are rows returned.
-    if rows:
-        # New empty list called 'result'. This will be written to a file.
-        result = list()    # The row name is the first entry for each entity in the description tuple.
-        column_names = list()
-        for row in rows:
-            result.append(row)    # Write result to file.
-        with open(csv_file_path, 'w', newline='') as csvfile:
-            csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            for row in result:
-                csvwriter.writerow(row)
-    else:
-        sys.exit("No rows found for query: {}".format(sql))
-
-if (dec == 1):
+#if (dec == 1):
     #Push Data
-
-    import mysql.connector
+def defineIllness(usid):
 
     mydb = mysql.connector.connect(
     host="165.227.33.15",
@@ -52,9 +18,9 @@ if (dec == 1):
 
     mycursor = mydb.cursor()
 
-    sql = "INSERT INTO data (Sick) VALUES (%s)"
+    sql = "UPDATE data SET sick = (%s) WHERE ID = " + str(usid)
 
-    insert = (input("Enter severity    "))
+    insert = (input("Enter severity"))
     val = (insert)
 
     mycursor.execute(sql, (val,))
@@ -62,3 +28,38 @@ if (dec == 1):
     mydb.commit()
 
     print(mycursor.rowcount, "record inserted.")
+
+#if (dec == 2):
+db_opts = {
+    'user': 'babysafe',
+    'password': 'FireFire',
+    'host': '165.227.33.15',
+    'database': 'babysafe'
+    }
+
+db = pymysql.connect(**db_opts)
+cur = db.cursor()
+
+sql = 'SELECT * FROM data WHERE sick IS NULL'
+csv_file_path = 'C:/Users/Sparsh/Desktop/my_csv_file.csv'
+usid = 0
+
+try:
+    cur.execute(sql)
+    rows = cur.fetchall()
+finally:
+    db.close()# Continue only if there are rows returned.
+if rows:
+    # New empty list called 'result'. This will be written to a file.
+    result = list()    # The row name is the first entry for each entity in the description tuple.
+    column_names = list()
+    for row in rows:
+        result.append(row)    # Write result to file.
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for row in result:
+            usid = row[0]
+            defineIllness(usid)
+            csvwriter.writerow(row)
+else:
+    sys.exit("No rows found for query: {}".format(sql))
